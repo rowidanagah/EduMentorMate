b48a3eb69fa3e690bb7e8faf05f7fcd60a72f0ac
Rowida46 <rowidanagah462000@gmail.com>
2023-02-19 16:48:47 +0200

    working on merge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/CustomNavbar';
 import Home from './pages/Home';
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
     <Navbar/>
     <Switch>
-      <Route exact path={"/"} component={Home}/>
+      <Route exact path={"/home"} component={Home}/>
     </Switch>
     </BrowserRouter>
   </div>
