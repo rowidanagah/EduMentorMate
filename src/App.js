a1d0df51fa90934818a5d148b6642c8b2779807b
Abdallah <Abdallah97158@gmail.com>
2023-02-25 23:29:00 +0200

    add Calener done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
-import GuestHome from './pages/GuestHome';
-import Navbar from './components/layout/Navbar';
-import Login from './pages/login';
-import Register from './pages/Registeration';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import GuestHome from "./pages/GuestHome";
+import Navbar from "./components/layout/Navbar";
+import Login from "./pages/login";
+import Register from "./pages/Registeration";
 //import Navbar from './components/CustomNavbar';
 
-import MentorProfile from './pages/MentorProfile';
-import CustomFooter from './components/layout/CustomFooter';
-import EditProfile from './components/profile/editProfile.js/EditProfile';
+import MentorProfile from "./pages/MentorProfile";
+import CustomFooter from "./components/layout/CustomFooter";
+import EditProfile from "./components/profile/editProfile.js/EditProfile";
 
-import Home from './pages/Home';
-import Footer from './components/layout/Footer';
+import Home from "./pages/Home";
+import Footer from "./components/layout/Footer";
+import { Calender } from "./pages/Calender";
 
 function App() {
   return (
-    <div style={{backgroundColor:"#ECE8DD"}}>
+    <div style={{ backgroundColor: "#ECE8DD" }}>
       <BrowserRouter>
         <Navbar />
         <Switch>
@@ -24,12 +25,12 @@ function App() {
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/mentorProfile" component={MentorProfile} />
+          <Route exact path="/calender" component={Calender} />
         </Switch>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
-
     </div>
-  )
+  );
 }
 // MentorProfile
 
---
+++ src/conts.js
@@ -0,0 +1,8 @@
+export const DAYS = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+
+export const MOCKAPPS = [
+  { date: new Date(2022, 9, 3), title: "Abdallah", color: "#238783" },
+  { date: new Date(2022, 9, 6), title: "Osama", color: "#708898" },
+  { date: new Date(2022, 9, 25), title: "youssef", color: "#047106" },
+  { date: new Date(2022, 9, 3), title: "karem", color: "#371395" },
+];
