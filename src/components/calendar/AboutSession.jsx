3e6bbb32530b7886beb56071a6008cba5bfacbf0
Abdallah <Abdallah97158@gmail.com>
2023-04-06 17:22:23 +0200

    fix session about

--- src/components/calendar/AboutSession.jsx
+++ src/components/calendar/AboutSession.jsx
@@ -1,24 +1,31 @@
+const AboutSession = () => {
+  return (
+    <div class="card mb-3">
+      <div class="card-body">
+        <h5
+          class="card-title"
+          style={{
+            textAlign: "center",
+            lineHeight: "5rem",
+            fontFamily: "sans-serif",
+            color: "#35b178",
+            fontSize: "2rem",
+          }}
+        >
+          you finally here select your favourt date from here{" "}
+        </h5>
+        <p class="card-text" style={{ color: "#afaf55" }}>
+          yellow session mean it available to book{" "}
+        </p>
+        <p class="card-text" style={{ color: "#75836e" }}>
+          gray session it reserved before you cant book{" "}
+        </p>
+        <p class="card-text">
+          <small class="text-body-secondary"></small>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-
-
-
-const AboutSession=()=>{
-
-
-  return <div class="card mb-3">
-  <div class="card-body">
-    <h5 class="card-title" style={{textAlign: 'center',
-lineHeight: '5rem',
-fontFamily: 'sans-serif',
-color: '#35b178',
-fontSize: '2rem'}}>you finaly here select your favourt date  </h5>
-    <p class="card-text" style={{color: '#afaf55'}}>yellow session  mean it available to reserve  </p>
-    <p class="card-text" style={{color:'#75836e'}}>gray session it reserved before you cant pick </p>
-    <p class="card-text"><small class="text-body-secondary"></small></p>
-  </div>
-</div>
-}
-
-
-
-export default AboutSession
+export default AboutSession;
